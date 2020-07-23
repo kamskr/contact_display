60476842901f923ed1a2c8f5bd3478b2fd1d4e25
Kamil Sikora <kamil.sikora@sklodowscy.pl>
2020-07-24 00:29:27 +0200

    adding global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import GlobalStyle from './theme/GlobalStyle';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Navbar />
     </div>
   );
---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    *, *::before, *::after {
+        box-sizing: border-box;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    html {
+        font-size: 62.5%;
+    }
+
+    body {
+        font-size: 1.6rem;
+    }
+`;
+
+export default GlobalStyle;
