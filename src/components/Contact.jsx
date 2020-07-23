43e7132d7d98e6b63f440802a6dab840a8ccad26
Kamil Sikora <kamil.sikora@sklodowscy.pl>
2020-07-24 01:57:41 +0200

    added axios api call

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div``;
+
+const Contact = (id, first_name, last_name, email, gender, avarar) => {
+  return (
+    <StyledContainer>
+      <h1>Contact</h1>
+    </StyledContainer>
+  );
+};
+
+export default Contact;
--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import SearchBar from './SearchBar';
+import Contact from './Contact';
+
+import axios from 'axios';
 
 export class MainView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      contacts: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+  async getData() {
+    axios
+      .get('https://teacode-recruitment-challenge.s3.eu-central-1.amazonaws.com/users.json')
+      .then((res) => {
+        this.setState({ contacts: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   render() {
+    const { contacts } = this.state;
+    console.log('Contacts', contacts);
     return (
       <div>
         <Navbar />
         <SearchBar />
+        {contacts ? (
+          contacts.map(({ first_name, last_name }) => {
+            console.log(first_name);
+            // <Contact first_name last_name />;
+          })
+        ) : (
+          <p>...Loading</p>
+        )}
+        <Contact />
       </div>
     );
   }
